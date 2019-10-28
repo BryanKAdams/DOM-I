7b50309bcd1c281f233abdcb07d85c37b4d0c116
BryanKAdams <17adamsbryk@washk12.org>
2019-10-28 14:57:24 -0600

    Site looks basically the same, not sure if I'm supposed to add the breaks in javascript for the contact street portion and the DOM is Awesome portion

--- js/index.js
+++ js/index.js
@@ -40,3 +40,73 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+//NAV STUFF
+let nav = document.getElementsByTagName('a');
+nav[0].textContent = siteContent["nav"]["nav-item-1"];
+nav[1].textContent = siteContent["nav"]["nav-item-2"];
+nav[2].textContent = siteContent["nav"]["nav-item-3"];
+nav[3].textContent = siteContent["nav"]["nav-item-4"];
+nav[4].textContent = siteContent["nav"]["nav-item-5"];
+nav[5].textContent = siteContent["nav"]["nav-item-6"];
+
+// Call to action Stuff
+let ctaText = document.querySelector(".cta-text h1");
+ctaText.textContent = siteContent["cta"]["h1"];
+let ctaButt = document.querySelector(".cta-text button");
+ctaButt.textContent = siteContent["cta"]["button"];
+
+//CTA image
+let cta = document.getElementById("cta-img");
+cta.setAttribute('src', siteContent["cta"]["img-src"])
+
+
+//Main Content
+
+//Top Content
+let featuresh4 = document.querySelector('.top-content .text-content h4');
+featuresh4.textContent = siteContent["main-content"]["features-h4"];
+let featuresp = document.querySelector('.top-content .text-content p');
+featuresp.textContent = siteContent["main-content"]["features-content"];
+let abouth4 = document.querySelector('.top-content .text-content:nth-child(2) h4');
+abouth4.textContent = siteContent["main-content"]["about-h4"];
+let aboutp = document.querySelector('.top-content .text-content:nth-child(2) p');
+aboutp.textContent = siteContent["main-content"]["about-content"];
+
+
+
+// Main content middle banner image
+let mainImg = document.getElementById("middle-img");
+mainImg.setAttribute('src', siteContent["main-content"]["middle-img-src"])
+
+//Bottom content
+let servicesh4 = document.querySelector('.bottom-content .text-content h4');
+servicesh4.textContent = siteContent["main-content"]["services-h4"];
+let servicesp = document.querySelector('.bottom-content .text-content p');
+servicesp.textContent = siteContent["main-content"]["services-content"];
+let producth4 = document.querySelector('.bottom-content .text-content:nth-child(2) h4');
+producth4.textContent = siteContent["main-content"]["product-h4"];
+let productp = document.querySelector('.bottom-content .text-content:nth-child(2) p');
+productp.textContent = siteContent["main-content"]["product-content"];
+let visionh4 = document.querySelector('.bottom-content .text-content:nth-child(3) h4');
+visionh4.textContent = siteContent["main-content"]["vision-h4"];
+let visionp = document.querySelector('.bottom-content .text-content:nth-child(3) p');
+visionp.textContent = siteContent["main-content"]["vision-content"];
+
+
+//contact section
+let contacth4 = document.querySelector('.contact h4');
+contacth4.textContent = siteContent["contact"]["contact-h4"];
+
+let contactp1 = document.querySelector('.contact p:nth-of-type(1)');
+contactp1.textContent = siteContent["contact"]["address"];
+let contactp2 = document.querySelector('.contact p:nth-of-type(2)');
+contactp2.textContent = siteContent["contact"]["phone"];
+let contactp3 = document.querySelector('.contact p:nth-of-type(3)');
+contactp3.textContent = siteContent["contact"]["email"];
+
+//footer section
+let footerp = document.querySelector('footer p');
+footerp.textContent = siteContent["footer"]["copyright"];
+
+
+
